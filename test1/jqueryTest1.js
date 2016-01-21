c459642d0eb68482cb6565d65ab9b9ea99addd4f
CallisteHanriat <couim38@gmail.com>
2016-01-21 22:57:57 +0100

    Some clearing improvments

--- test1/jqueryTest1.js
+++ test1/jqueryTest1.js
@@ -1,11 +1,11 @@
 $(function() {
    $('title').html('test1');
-   $('.caseOfText').mouseover((this),function(){
+   $('.caseOfText').mouseover(function(){
         var item=$(this);
         item.css({'background-color':'yellow'});
    }); 
    
-   $('.caseOfText').mouseleave((this), function() {
+   $('.caseOfText').mouseleave(function() {
       var item=$(this);
       item.css({'background-color':'rgb(255,255,255)'});
    });
