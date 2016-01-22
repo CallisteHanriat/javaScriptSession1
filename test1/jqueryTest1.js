c53e182993edab11e6c24d022bbd5ba497d0ed1e
CallisteHanriat <couim38@gmail.com>
2016-01-22 13:21:07 +0100

    avancement

--- test1/jqueryTest1.js
+++ test1/jqueryTest1.js
@@ -1,19 +1,37 @@
 $(function() {
    $('title').html('test1');
-   $('.caseOfText').mouseover(function(){
+   $('#myTable').on('mouseover', '.caseOfText', function(){
         var item=$(this);
         item.animate({'background-color':'yellow'}, 250);
-   }); 
-   
-   $('.caseOfText').mouseleave(function() {
+   });
+
+   $('#myTable').on('mouseleave', '.caseOfText', function() {
       var item=$(this);
       item.animate({
           backgroundColor: "white"
         }, 250 );
    });
-   
-   $('<button/>').html('add')
+
+   $('#myTable').on('click', '.caseOfText', function() {
+      var item=$(this);
+      item.animate({
+          backgroundColor: "white"
+        }, 250 );
+   });
+
+   var buttonAdd = $('<button/>').html('add')
            .css({'margin':'auto',
             'display':'block'})
            .appendTo('body');
+
+    buttonAdd.click(function() {
+        $('<tr/>')
+            .append($('<td/>')
+                .text('ThirdElement')
+                .addClass('caseOfText'))
+            .append($('<td/>')
+                .text('FourthElement')
+                .addClass('caseOfText'))
+            .appendTo('table');
+    });
 });
